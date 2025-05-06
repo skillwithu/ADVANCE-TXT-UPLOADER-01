b034db2bac6e85f08b0d209bf17cf4275a65a25e
BADFELLA CHAHAL <ku9690845@gmail.com>
2025-05-06 12:27:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -625,7 +625,7 @@ async def upload(bot: Client, m: Message):
             elif '/master.mpd' in url:
              id =  url.split("/")[-2]
              #url = f"https://anonymouspwplayer-b99f57957198.herokuapp.com/pw?url={url}?token={raw_text4}"
-             url = f"https://madxabhi-pw.onrender.com/{id}/master.m3u8?token={raw_text4}"
+             url = f"https://anonymouspwplayer-b99f57957198.herokuapp.com/pw?url=%7Burl%7D?token=%7Bpw_token%7D"
             #elif '/master.mpd' in url:
              #id =  url.split("/")[-2]
              #url = f"https://dl.alphacbse.site/download/{id}/master.m3u8"
